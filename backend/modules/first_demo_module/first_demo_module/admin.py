c2cde2423b3d3c0469aee599fd287202b3a61dea
nilaysaxena <93970439+nilaysaxena@users.noreply.github.com>
2023-10-31 14:39:45 +0530

    Update admin.py

--- backend/modules/first_demo_module/first_demo_module/admin.py
+++ backend/modules/first_demo_module/first_demo_module/admin.py
@@ -4,4 +4,4 @@ from .models import TermAndCondition
 
 @admin.register(TermAndCondition)
 class TermAndConditionAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'is_active', 'type']
+    list_display = ['__str__', 'is_active']
